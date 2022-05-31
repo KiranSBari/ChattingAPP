a44175c5dceaf2c03b5cd1362f27d7e52106f791
Kiran Bari <kiran.bari@virtuaresearch.com>
2022-05-31 05:32:57 +0530

    Created home module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HedderComponent } from './home/hedder/hedder.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { ContainerComponent } from './home/container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HedderComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/home/hedder/hedder.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HedderComponent } from './hedder.component';
+
+describe('HedderComponent', () => {
+  let component: HedderComponent;
+  let fixture: ComponentFixture<HedderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HedderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HedderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
